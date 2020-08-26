06d4e7f1bb27e5f75d0473f33d776ecbb370a48e
Vera Var <primavera1919@yandex.ru>
2020-08-26 18:41:55 +0300

    Test added

---
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -0,0 +1,23 @@
+package ru.netology;
+
+import com.codeborne.selenide.SelenideElement;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardOrderTest {
+
+    @Test
+    void shouldReturnOK() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Салтыков-Щедрин Михаил");
+        form.$("[data-test-id=phone] input").setValue("+70123456789");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $("[data-test-id=order-success]").shouldHave(text("Ваша заявка успешно отправлена!"));
+    }
+}
